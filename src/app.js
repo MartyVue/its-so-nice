e2091908c3b71af5149882fba1c611bf655742a5
MartyVue <84153688+MartyVue@users.noreply.github.com>
2021-05-30 14:58:08 -0600

    edit date and time

--- src/app.js
+++ src/app.js
@@ -1,2 +1,22 @@
-// alert("Hello world")
-console.log("hi");
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description")
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "5ef560c2739fa62b5e22bb83083603a3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Broomfield&appid=${apiKey}&units=metric`; 
+
+axios.get(apiUrl).then(displayTemperature);
