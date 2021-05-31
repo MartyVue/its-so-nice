af5f89f1a38f17df8714df1e8e737c1f934fd050
MartyVue <84153688+MartyVue@users.noreply.github.com>
2021-05-31 00:02:27 -0600

    Add form and button

--- src/app.js
+++ src/app.js
@@ -32,8 +32,20 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "5ef560c2739fa62b5e22bb83083603a3";
-let city = "Paris"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+function search(city) {
+    let apiKey = "5ef560c2739fa62b5e22bb83083603a3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("city-input");
+    search(cityInputElement.value);
+}
+
+search("Broomfield");
+
+
+let form = document.querySelector("search-form");
+form.addEventListener("sumbit", handleSubmit);
