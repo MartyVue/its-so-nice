d9abc19ae6e9dca55306cbed6f8b87dfda55ec57
MartyVue <84153688+MartyVue@users.noreply.github.com>
2021-06-06 22:12:45 -0600

    fix index to show only 6 days of the week

--- src/app.js
+++ src/app.js
@@ -29,7 +29,8 @@ function displayForecast(response) {
     
     let forecastHTML = `<div class="row">`;
   
-    forecast.forEach(function(forecastDay) {
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
     forecastHTML = 
         forecastHTML + `
             <div class="col-2">
@@ -46,6 +47,7 @@ function displayForecast(response) {
                   </div>
             </div>
         `;
+        }
     }); 
 
         forecastHTML = forecastHTML + `</div>`;
